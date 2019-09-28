7fb52519f32935421e1e786d965299a29263c63a
Victoria Moore <victoriamoore563@gmail.com>
2019-09-28 12:53:40 -0700

    add Q's and A's in STORE

--- store.js
+++ store.js
@@ -1,42 +1,58 @@
 const STORE = {
     questions: [
         {
-            question:
+            question: 'Who is the Doctors greatest adversary?'
             options: [
-
+                'Daleks',
+                'The Master',
+                'Cybermen',
+                'Weeping Angels'
             ],
-            answer:
+            answer: 'The Master'
         },
 
         {
-            question:
+            question: 'Which species say’s “Exterminate!” most?',
             options: [
-
+                'Daleks',
+                'Sontarans',
+                'Cybermen',
+                'Sea Devils'
             ],
-            answer:
+            answer: 'Daleks'
         },
 
         {
-            question:
+            question: 'Who steals humans so that they can undergo "Upgrading"?',
             options: [
-
+                'Time Lords',
+                'Ice Warriors',
+                'Silents',
+                'Cybermen'
             ],
-            answer: 
+            answer: 'Cybermen'
         },
 
         {
-            question: 
+            question: 'What does TARDIS stand for?',
             options: [
-
+                'Tardigrades in Space',
+                'Tethered Aerial Release Developed in Style',
+                'Time and Relative Dimention in Space',
+                'Tornado Advanced Radar Display Information System'
             ],
-            answer:
+            answer: 'Time and Relative Dimention in Space'
         },
 
         {
-            question: 
+            question: 'How many regenerations has the Doctor gone through?',
             options: [
-
+                '13',
+                '11',
+                '10',
+                '14'
             ],
+            answer: '14'
         },
     ]
 }
