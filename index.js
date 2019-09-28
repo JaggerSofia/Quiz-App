add65fe4a9bdb60c2ff9e987eaf83978e44e6a04
Victoria Moore <victoriamoore563@gmail.com>
2019-09-28 13:46:20 -0700

    initial psuedocode into index.js

---
+++ index.js
@@ -0,0 +1,49 @@
+function startQuiz() {
+    //this function will create an event listener that 
+    //allow the start button to move to the first
+    //question by using a click event
+}
+
+function questionNumberAndScore() {
+    //this will update the score on whichever page the 
+    //quiz is located on based on how the questions are 
+    //answered
+}
+function updateQuestionOptions() {
+    //this will render the <radio> options for the quiz
+    //depending on which question is being rendered
+}
+
+function renderQuestions() {
+    //this function will place a new question on the screen
+    //will also update the current ? number and score
+}
+
+function finalResult() {
+    //this will be where the final score will be displayed when all ?
+    //have been answered 
+}
+
+function handleQuestion() {
+    //This is where I will add an event listener to the next question
+    //button at the botton of each page
+}
+
+function optionSelections() {
+    //this function will have most of the content
+    //it will have an even listener of submit so that the answer can
+    //be read. It will also conatin what will display on the screen 
+    //depending on the answer selected
+}
+
+function restartQuiz() {
+    //create an event listener that will allow  the user
+    // to restart the quiz when they have completed it
+}
+
+function handleQuiz() {
+    startQuiz();
+    renderQuestions();
+    optionSelections();
+    restartQuiz();
+}
